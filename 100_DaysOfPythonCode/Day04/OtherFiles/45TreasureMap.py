d84fa3ab31e7d2df0a746bb4d8c381a2641cc20e
Utkarshchandra24 <78264853+Utkarshchandra24@users.noreply.github.com>
2024-03-09 18:38:02 +0530

    Add files via upload

--- 100_DaysOfPythonCode/Day04/OtherFiles/45TreasureMap.py
+++ 100_DaysOfPythonCode/Day04/OtherFiles/45TreasureMap.py
@@ -7,7 +7,11 @@ map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
 position = input("Where do you want to put the treasure? ")
 
-user_input =
+horizontal = int(position[0])
+vertical = int(position[1])
+
+selected_row = map[vertical - 1]
+selected_row[horizontal - 1] = "X"
 
 
 
--- 100_DaysOfPythonCode/Day04/RockPaperScissors2.py
+++ 100_DaysOfPythonCode/Day04/RockPaperScissors2.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -24,4 +26,28 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
+game_images = [rock,paper,scissors]
+
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. \n"))
+if user_choice >=3 or user_choice<0:
+    print("You typed an invalid number,you lose!")
+else:
+    print(game_images[user_choice])
+
+    computer_choice = random.randint(0,2)
+    print("Computer chose:")
+    print(game_images[computer_choice])
+    # print(f"Computer chose {computer_choice}")
+
+
+    if user_choice==0 and computer_choice==2:
+        print("You Win!")
+    elif computer_choice > user_choice:
+        print("You Lose!")
+    elif computer_choice == user_choice:
+        print("Its a Draw")
+    elif user_choice > computer_choice:
+        print("You Lose!")
+
 
